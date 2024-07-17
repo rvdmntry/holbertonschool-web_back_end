b6737f436b36157b793ddc3d8523cdf4b8f508a7
ramon <r.antovega@gmail.com>
2024-07-17 19:52:51 -0400

    Object property value shorthand syntax

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,10 @@
 // 7-getBudgetObject.js
 export default function getBudgetObject(income, gdp, capita) {
 	const budget = {
-	  income,
-	  gdp,
-	  capita,
-	};
+	income,
+	gdp,
+	capita,
+    }
 
 	return budget;
   }
