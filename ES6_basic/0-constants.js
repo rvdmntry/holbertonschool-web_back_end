98f2127adabb1c3ab7689ba018e285f60a6a6562
ramon <r.antovega@gmail.com>
2024-07-17 12:34:13 -0400

    Const or let

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,4 @@
 // 0-constants.js
-
 export function taskFirst() {
 	const task = 'I prefer const when I can.';
 	return task;
@@ -12,6 +11,5 @@ export function taskFirst() {
   export function taskNext() {
 	let combination = 'But sometimes let';
 	combination += getLast();
-
 	return combination;
   }
