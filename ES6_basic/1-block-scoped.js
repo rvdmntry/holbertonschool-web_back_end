e54c754ad25b243849deaa420b12497808f22b81
ramon <r.antovega@gmail.com>
2024-07-17 19:18:11 -0400

    Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 // 1-block-scoped.js
 export default function taskBlock(trueOrFalse) {
-	const task = false;
-	const task2 = true;
+  const task = false;
+  const task2 = true;
 
-	if (trueOrFalse) {
-	  const task = true; // block-scoped
-	  const task2 = false; // block-scoped
-	}
+  if (trueOrFalse) {
+  const task = true; // block-scoped
+  const task2 = false; // block-scoped
+}
 
-	return [task, task2];
+return [task, task2];
   }
