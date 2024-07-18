a94d9af4d63270e4d228d3818a6f4610294ca754
ramon <r.antovega@gmail.com>
2024-07-17 20:14:59 -0400

    No need to create empty objects before adding in properties

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,15 @@
-// 8-getBudgetCurrentYear.js
 function getCurrentYear() {
 	const date = new Date();
 	return date.getFullYear();
   }
 
   export default function getBudgetForCurrentYear(income, gdp, capita) {
-	const year = getCurrentYear();
-	const budget = {
-	  [`income-${year}`]: income,
-	  [`gdp-${year}`]: gdp,
-	  [`capita-${year}`]: capita,
-	};
+  const year = getCurrentYear();
+  const budget = {
+[`income-${year}`]: income,
+[`gdp-${year}`]: gdp,
+[`capita-${year}`]: capita,
+  };
 
-	return budget;
-  }
+  return budget;
+}
