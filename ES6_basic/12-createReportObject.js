0a19b9baa05509b96140479efd03d360f9bbf9a3
ramon <r.antovega@gmail.com>
2024-07-17 20:37:13 -0400

    let create a report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+// 12-createReportObject.js
+export default function createReportObject(employeesList) {
+	return {
+	  allEmployees: {
+		...employeesList,
+	  },
+	  getNumberOfDepartments(employees) {
+		return Object.keys(employees).length;
+	  },
+	};
+  }
