e1c498b1f05dc44407bd1a3e55fd67a949209cdc
ramon <r.antovega@gmail.com>
2024-07-21 11:54:01 -0400

    Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,53 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw TypeError('Amount must be a number');
+    }
+
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw TypeError('Currency must be an instance of Currency');
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw TypeError('Currency must be an instance of Currency');
+    }
+  }
+
+  displayFullPrice() {
+    const { name, code } = this._currency;
+    return `${this._amount} ${name} (${code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw TypeError('Amount and conversion rate must be numbers');
+  }
+}
