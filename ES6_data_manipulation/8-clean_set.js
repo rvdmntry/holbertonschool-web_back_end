b1a5e07e335f7bb34b4bd1117e62f733910eb1bd
ramon <r.antovega@gmail.com>
2024-07-22 16:51:58 -0400

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
     return '';
   }
- 
+
   const filteredValues = [];
   for (const value of set) {
     if (value.startsWith(startString)) {
