54c395fe7959c7db965e3f2bcd6a490bd9e15ec4
ramon <r.antovega@gmail.com>
2024-07-22 16:18:15 -0400

    Filter

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
-export default function getListStudentIds(arr) {
-  if (!Array.isArray(arr)) {
-    return [];
-  }
-  return arr.map(student => student.id);
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
 }
