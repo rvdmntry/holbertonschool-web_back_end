3ca2cd43e4e4214b7cfc40f886219b20f72af436
ramon <r.antovega@gmail.com>
2024-07-25 13:20:38 -0400

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then (results => results.map (result => ({
+    .then(results => results.map(result => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
     })));
