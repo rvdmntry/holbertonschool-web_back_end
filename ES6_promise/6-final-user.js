0b1664d155ab102eb0e269da7e99ca2dbbb076fd
ramon <r.antovega@gmail.com>
2024-07-25 12:52:38 -0400

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,3 +1,24 @@
-import handleProfileSignup from './6-final-user';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg").then(console.log;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUp, upload])
+    .then((results) => {
+      return results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return {
+            status: result.status,
+            value: result.value,
+          };
+        } else {
+          return {
+            status: result.status,
+            value: result.reason,
+          };
+        }
+      });
+    });
+}
