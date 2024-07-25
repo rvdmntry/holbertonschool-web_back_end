9e054c5d11b9d64a9ea62ac6a5981766ea49c1b3
ramon <r.antovega@gmail.com>
2024-07-25 12:36:11 -0400

    initial

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,10 @@
-export default function getBudgetObject(income, gdp, capita) {
-	const budget = {
-    income,
-    gdp,
-    capita,
-    };
-
-    return budget;
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
 }
